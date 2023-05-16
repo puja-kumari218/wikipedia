6d800b8642f40c0471be4bec5fcffc5a0bc3d717
puja kumari <pkumari41038@gmail.com>
2023-05-16 12:13:41 +0530

    first commit

---
+++ wiki-url.js
@@ -0,0 +1,8 @@
+
+const url =
+  'https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=&srlimit=20&srsearch=SEARCH_QUERY_GOES_HERE';
+
+// format=json json response
+// "origin=*" fix cors errors
+
+const page_url = 'https://en.wikipedia.org/wiki/Help:Link';
